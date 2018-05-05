9c1e8a9aab4c05c988df1588e2a46adf8e077f28
ScorpioMiku <1056992492@qq.com>
2018-05-05 22:29:44 +0800

    Initial commit

---
+++ app/src/main/java/wuxiang/miku/scorpio/wuxiang/base/BaseFragment.java
@@ -0,0 +1,160 @@
+package wuxiang.miku.scorpio.wuxiang.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by Wangtianrui on 2018/5/5.
+ */
+
+public abstract class BaseFragment extends Fragment {
+
+    private View parentView;
+    private FragmentActivity activity;
+    private Unbinder bind;
+    protected boolean isVisible;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        parentView = inflater.inflate(getLayoutId(), container, false);
+        activity = getSupportActivity();
+        return parentView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        bind = ButterKnife.bind(this, view);
+        finishCreateView(savedInstanceState);
+    }
+
+    public abstract int getLayoutId();
+
+    public FragmentActivity getSupportActivity() {
+        return super.getActivity();
+    }
+
+    public abstract void finishCreateView(Bundle state);
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        bind.unbind();
+    }
+
+    /**
+     * 得到当前fragment后面的activity
+     *
+     * @param context
+     */
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.activity = (FragmentActivity) context;
+    }
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        this.activity = null;
+    }
+
+
+    public Context getApplicationContext() {
+        return this.activity == null ? (getActivity() == null ?
+                null : getActivity().getApplicationContext()) : this.activity.getApplicationContext();
+    }
+
+    /**
+     * Fragment数据的懒加载
+     */
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (getUserVisibleHint()) {
+            isVisible = true;
+            onVisible();
+        } else {
+            isVisible = false;
+            onInvisible();
+        }
+    }
+
+    /**
+     * fragment显示时才加载数据
+     */
+    protected void onVisible() {
+        lazyLoad();
+    }
+
+    /**
+     * fragment懒加载方法
+     */
+    protected void lazyLoad() {
+    }
+
+    /**
+     * fragment隐藏
+     */
+    protected void onInvisible() {
+    }
+
+    /**
+     * 加载数据
+     */
+    protected void loadData() {
+    }
+
+    /**
+     * 显示进度条
+     */
+    protected void showProgressBar() {
+    }
+
+    /**
+     * 隐藏进度条
+     */
+    protected void hideProgressBar() {
+    }
+
+    /**
+     * 初始化recyclerView
+     */
+    protected void initRecyclerView() {
+    }
+
+    /**
+     * 初始化refreshLayout
+     */
+    protected void initRefreshLayout() {
+    }
+
+    /**
+     * 设置数据显示
+     */
+    protected void finishTask() {
+    }
+
+
+    @SuppressWarnings("unchecked")
+    public <T extends View> T $(int id) {
+        return (T) parentView.findViewById(id);
+    }
+}
---
+++ app/src/main/java/wuxiang/miku/scorpio/wuxiang/modules/EatingFragment.java
@@ -0,0 +1,10 @@
+package wuxiang.miku.scorpio.wuxiang.modules;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Wangtianrui on 2018/5/5.
+ */
+
+public class EatingFragment extends Fragment {
+}
