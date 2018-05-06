91a76fd1350d53fff86cecf9fcc41aaead0d2d2d
ScorpioMiku <1056992492@qq.com>
2018-05-06 21:18:50 +0800

    1

--- app/src/main/java/wuxiang/miku/scorpio/wuxiang/modules/main/MainActivity.java
+++ app/src/main/java/wuxiang/miku/scorpio/wuxiang/modules/main/MainActivity.java
@@ -1,17 +1,23 @@
 package wuxiang.miku.scorpio.wuxiang.modules.main;
 
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.View;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
+import butterknife.BindView;
 import wuxiang.miku.scorpio.wuxiang.R;
 import wuxiang.miku.scorpio.wuxiang.base.BaseActivity;
 import wuxiang.miku.scorpio.wuxiang.modules.eating.EatingFragment;
+import wuxiang.miku.scorpio.wuxiang.utils.ToastUtils;
 
 public class MainActivity extends BaseActivity {
+    @BindView(R.id.floationg_action_menu)
+    FloatingActionsMenu floationgActionMenu;
     private Fragment[] fragments;
     private EatingFragment eatingFragment;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,7 @@ public class MainActivity extends BaseActivity {
     @Override
     public void initViews(Bundle savedInstanceState) {
         initFragments();
+        initFloatingMenu();
     }
 
     @Override
@@ -51,5 +58,20 @@ public class MainActivity extends BaseActivity {
                 .beginTransaction()
                 .add(R.id.container_main, eatingFragment)
                 .show(eatingFragment).commit();
+    }
+
+    private void initFloatingMenu() {
+        FloatingActionButton actionButtonActivate = new FloatingActionButton(getBaseContext());
+        actionButtonActivate.setTitle("实物AR扫描");
+//        actionButtonActivate.setSize(FloatingActionButton.SIZE_MINI);
+        actionButtonActivate.setIcon(R.drawable.ic_ar);
+        actionButtonActivate.setTextDirection(FloatingActionButton.TEXT_DIRECTION_FIRST_STRONG_LTR);
+        actionButtonActivate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtils.showShortToast("纸悦动");
+            }
+        });
+        floationgActionMenu.addButton(actionButtonActivate);
     }
 }
