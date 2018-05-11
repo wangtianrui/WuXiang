e793da4b1e1cbbbf399b5c57e0a00bdd1accabda
ScorpioMiku <1056992492@qq.com>
2018-05-11 14:30:08 +0800

    5-11

--- rrmlib/src/main/java/com/cicinnus/retrofitlib/hybrid/jsbridge/BridgeHandler.java
+++
@@ -1,7 +0,0 @@
-package com.cicinnus.retrofitlib.hybrid.jsbridge;
-
-public interface BridgeHandler {
-	
-	void handler(String data, CallBackFunction function);
-
-}
--- rrmlib/src/main/java/com/cicinnus/retrofitlib/hybrid/jsbridge/CallBackFunction.java
+++
@@ -1,7 +0,0 @@
-package com.cicinnus.retrofitlib.hybrid.jsbridge;
-
-public interface CallBackFunction {
-	
-	public void onCallBack(String data);
-
-}
